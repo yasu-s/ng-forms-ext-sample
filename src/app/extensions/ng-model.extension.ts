355c668589897ee3af07e4e627894b45f1092474
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-01-13 18:24:42 +0900

    拡張追加

---
+++ src/app/extensions/ng-model.extension.ts
@@ -0,0 +1,19 @@
+export {};
+
+import { NgModel } from '@angular/forms';
+
+declare module '@angular/forms/src/directives/ng_model' {
+
+  interface NgModel {
+    /**
+     * NgModel.touched or dirty
+     * @returns touched or dirty
+     */
+    touchedOrDirty(): boolean;
+  }
+}
+
+NgModel.prototype.touchedOrDirty = function() {
+  const control = this as NgModel;
+  return control.touched || control.dirty;
+};
