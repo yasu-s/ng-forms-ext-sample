f8f55101fb15327b5fe59c1c04e76a84ec2dcadd
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-01-13 18:30:57 +0900

    画面設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-forms-ext-sample';
+  text1 = '';
+  text2 = 'aaa';
 }
